aca155d1f49afd2801a4c59d8c3cc66835afb74f
Takaki <takaki55730317@gmail.com>
2020-01-26 19:09:55 +0900

    クラス継承時のクラスインスタンス変数とインスタンス変数の管理のされ方

--- practice_variable.rb
+++ practice_variable.rb
@@ -24,3 +24,54 @@ Hoge.foo #=> "Bar"
 hoge = Hoge.new('Baz')
 hoge.foo #=> "Baz"
 Hoge.foo #=> "Bar"
+
+# --------------------------------------------------------------------
+
+# クラスインスタンス変数とインスタンス変数 FugaクラスはHogeクラスを継承
+
+class Hoge
+  # クラスインスタンス変数
+  @foo = 'Bar'
+
+  def self.foo
+    # クラスインスタンス変数
+    @foo
+  end
+
+  def initialize(foo)
+    # インスタンス変数
+    @foo = foo
+  end
+
+  def foo
+    # インスタンス変数
+    @foo
+  end
+end
+
+class Fuga < Hoge
+  @foo = 'Fizz'
+
+  def self.foo
+    # クラスインスタンスを参照
+    @foo
+  end
+
+  def upcase_foo
+    # インスタンス変数を参照
+    @foo.upcase
+  end
+end
+
+Hoge.foo #=> "Bar"
+Fuga.foo #=> "Fizz"
+
+hoge = Hoge.new('Baz')
+hoge.foo #=> "Baz"
+
+fuga = Fuga.new('FizzBuzz')
+fuga.foo        #=> "FizzBuzz"
+fuga.upcase_foo #=> "FIZZBUZZ"
+
+Hoge.foo        #=> "Bar"
+Fuga.foo        #=> "Fizz"
